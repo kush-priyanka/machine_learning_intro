fdb8f0c3d5724d2bca66a9fe2af2be607cf17e9f
Priyanka-laptop <pkushwaha@email.arizona.edu>
2022-07-21 12:28:45 -0700

    first nueral network example

--- 1-neural_network.R
+++ 1-neural_network.R
@@ -1,10 +1,11 @@
 ## Install packages
-install.packages('vioplot')
+install.packages('mice')
 install.packages('ISLR')
 install.packages('caTools')
 install.packages('neuralnet')
 
 ## Load the package
+library(mice) ##package to visualize missing data values
 library(ISLR) ## package with the college dataset
 library(caTools) ## used to split data into training and test
 library(neuralnet) #neural network function
@@ -45,11 +46,11 @@ marginplot(College[c("Apps", "Accept")],
            pch = c(20), col= c("darkgray","red","blue"))
 
 #### Neural Network ####
-## Create max and min to scale the data
+## Create max and min to normalize the data
 maxs <- apply(College[,2:18], 2, max)
 mins <- apply(College[,2:18], 2, min)
 
-## Scale the data
+## Scale/normalize the data
 scaled.data <- as.data.frame(scale(College[,2:18], 
                                    center = mins, 
                                    scale = maxs-mins))
@@ -62,11 +63,70 @@ data <- cbind(Private, scaled.data)
 
 set.seed(101) # sets random number
 
-# split data
+# split data, 70 % as training
 split <- sample.split(data$Private, SplitRatio = 0.70)
 
 # Split based off split Boolean Vector
 train <- subset(data, split == TRUE)
 test <- subset(data, split == FALSE)
 
+# Nueral network function; create a function
+feats <- names(scaled.data)
+f <- paste(feats , collapse = '+')
+f <- paste('Private~' , f)
+f <- as.formula(f)
 
+# Output of formula below
+# Private ~ Apps + Accept + Enroll + Top10perc + Top25perc + F.Undergrad + 
+#   P.Undergrad + Outstate + Room.Board + Books + Personal + 
+#   PhD + Terminal + S.F.Ratio + perc.alumni + Expend + Grad.Rate
+
+# Run the model
+nn <- neuralnet(f, train, 
+                hidden = c(10,10,10), 
+                linear.output = FALSE)
+
+# Compute predictions of test set
+predicted.nn.values <- compute(nn, test[2:18])
+print(head(predicted.nn.values$net.result))
+
+# Round off vlaues
+predicted.nn.values$net.result <- sapply(predicted.nn.values$net.result,
+                                         round, digits = 0)
+
+# Create a simple confusion matrix to check the trainer against test
+table(test$Private, 
+      predicted.nn.values$net.result)
+
+#              Predicted No    Predicted Yes
+# Actual No     TN=55           FP=9
+# Actual Yes    FN=6            TP =163
+
+## Visualize the plot
+# Black lines represent weighted vectors between nuerons.
+# Blue lines represents the bias added.
+plot(nn)
+
+## Run the model on 80% of the data as training
+split1 <- sample.split(data$Private, SplitRatio = 0.80)
+
+# Split based off split Boolean Vector
+train1 <- subset(data, split1 == TRUE)
+test1 <- subset(data, split1 == FALSE)
+
+# Run the model
+nn1 <- neuralnet(f, train1, 
+                hidden = c(10,10,10), 
+                linear.output = FALSE)
+
+# Compute predictions of test set
+predicted.nn.values1 <- compute(nn1, test1[2:18])
+print(head(predicted.nn.values1$net.result))
+
+# Round off vlaues
+predicted.nn.values1$net.result <- sapply(predicted.nn.values1$net.result,
+                                         round, digits = 0)
+
+# Create a simple confusion matrix to check the trainer against test
+table(test1$Private, 
+      predicted.nn.values1$net.result)
